38454b88a03ca28d9e520687d7c4a500c983db8d
Lauren Days <lmchambers0420@gmail.com>
2018-09-14 22:28:57 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,11 @@
 def turn_count(board)
   counter = 0
-  board.each do
+  board.each do |i|
+    if i == "X" || i == "O"
+      counter += 1
+    end
+  end
+  return counter
 end
 
 def current_player(board)
